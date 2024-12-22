88b9b1aaec24e64218fe608c3d3285cbf44aa727
gparnalis <gparnalis@gmail.com>
2024-12-23 01:49:54 +0200

    added functions, small changes in main

--- src/algorithms/compactForwards.py
+++ src/algorithms/compactForwards.py
@@ -1,2 +1,18 @@
-def compactForwards():
-    print('Running Compact Forwards Algorithm...')
+import time
+
+def compactForwards(graph):
+    print('\nRunning Compact Forwards Algorithm...')
+    start_time = time.time()
+    triangles = 0
+    # Order nodes
+    nodes = sorted(graph.nodes())  #sort by node ID
+    node_index = {node: idx for idx, node in enumerate(nodes)}
+
+    for node in nodes:
+        neighbors = [n for n in graph.neighbors(node) if node_index[n] > node_index[node]]
+        for i in range(len(neighbors)):
+            for j in range(i + 1, len(neighbors)):
+                if graph.has_edge(neighbors[i], neighbors[j]):
+                    triangles += 1
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return triangles
--- src/algorithms/triplets.py
+++ src/algorithms/triplets.py
@@ -1,2 +1,13 @@
-def triplets():
-    print('Running Triplets Algorithm...')
+from itertools import combinations
+import time
+
+def triplets(graph):
+    print('\nRunning Triplets Algorithm...')
+    start_time = time.time()
+    triangles = 0
+    nodes = list(graph.nodes())
+    for u, v, w in combinations(nodes, 3):  #all triplets
+        if graph.has_edge(u, v) and graph.has_edge(v, w) and graph.has_edge(w, u):
+            triangles += 1
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return triangles
